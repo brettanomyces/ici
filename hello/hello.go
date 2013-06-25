e553173dcf4f359a7b579ecb5dee833169e5f178
brettyukich <brettyukich@gmail.com>
2013-06-25 21:41:32 +1200

    updated hello to use my 'newmath' package

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/brettyukich/ici/newmath"
+)
 
 func main() {
 	fmt.Printf("Hello Brett.\n")
+	fmt.Printf("Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
